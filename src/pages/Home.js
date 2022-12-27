dd9eeb54db41c7eb3755d61e35d9b20507650202
CelesTech03 <celeste.urena@stu.bmcc.cuny.edu>
2022-12-27 12:35:17 -0500

    Made icons clickable and added projects images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,11 @@ import DescriptionIcon from '@material-ui/icons/Description'
 import '../styles/Home.css'
 import ProjectItem from '../components/ProjectItem'
 import Bookworm from '../images/bookworm.png'
+import Ems from '../images/EMS.png'
+import Droplet from '../images/droplet.png'
+import Restaurant from '../images/restaurant.png'
+import Twitter from '../images/twitter.png'
+import Instagram from '../images/instagram.png'
 
 function Home() {
   return (
@@ -15,17 +20,22 @@ function Home() {
         <h2>Hi, I'm Celeste Ureña</h2>
         <div className='prompt'>
           <p>Aspring Software Engineer</p>
-          <LinkedInIcon />
-          <DescriptionIcon />
-          <EmailIcon />
-          <GithubIcon />
+          <LinkedInIcon onClick={() => window.open('https://www.linkedin.com/in/celesteurena/')}/>
+          <DescriptionIcon onClick={() => window.open('https://drive.google.com/file/d/1GqhmS3FrLpN8TvWh6FNjL3tZVVxyk9oC/view?usp=sharing')}/>
+          <EmailIcon onClick={() => window.open('mailto:celeste.urena03@gmail.com')}/>
+          <GithubIcon onClick={() => window.open('https://github.com/CelesTech03')}/>
         </div>
       </div>
 
       <div className='projects'>
-        <h1>My Personal Projects</h1>
+        <h1>My Projects</h1>
         <div className='projectList'>
           <ProjectItem name="Book Finder App" image={Bookworm} />
+          <ProjectItem name="Employee Management System" image={Ems} />
+          <ProjectItem name="Game Website" image={Droplet} />
+          <ProjectItem name="Restaurant Website" image={Restaurant} />
+          <ProjectItem name="Twitter Clone App" image={Twitter} />
+          <ProjectItem name="Instagram Clone App" image={Instagram} />
         </div>
       </div>
     </div>
