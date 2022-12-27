fe6bd2b1672713e09f1213bb723e1c710e8043d1
CelesTech03 <celeste.urena@stu.bmcc.cuny.edu>
2022-12-26 23:55:49 -0500

    Projects section + CSS. Added one project.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,20 +2,32 @@ import React from 'react'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
 import EmailIcon from '@material-ui/icons/Email'
 import GithubIcon from '@material-ui/icons/GitHub'
+import DescriptionIcon from '@material-ui/icons/Description'
 import '../styles/Home.css'
+import ProjectItem from '../components/ProjectItem'
+import Bookworm from '../images/bookworm.png'
 
 function Home() {
   return (
     <div className='home'>
-        <div className='about'>
+
+      <div className='about'>
         <h2>Hi, I'm Celeste Ureña</h2>
         <div className='prompt'>
-            <p>Aspring Software Engineer</p>
-            <LinkedInIcon/>
-            <EmailIcon/>
-            <GithubIcon/>
+          <p>Aspring Software Engineer</p>
+          <LinkedInIcon />
+          <DescriptionIcon />
+          <EmailIcon />
+          <GithubIcon />
         </div>
+      </div>
+
+      <div className='projects'>
+        <h1>My Personal Projects</h1>
+        <div className='projectList'>
+          <ProjectItem name="Book Finder App" image={Bookworm} />
         </div>
+      </div>
     </div>
   )
 }
