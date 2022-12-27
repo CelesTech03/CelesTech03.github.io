cc458366c1f26b101174c5a1466bc7955838679f
CelesTech03 <celeste.urena@stu.bmcc.cuny.edu>
2022-12-27 13:13:20 -0500

    Added github links to projects + updated about me

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,23 +19,31 @@ function Home() {
       <div className='about'>
         <h2>Hi, I'm Celeste Ureña</h2>
         <div className='prompt'>
-          <p>Aspring Software Engineer</p>
-          <LinkedInIcon onClick={() => window.open('https://www.linkedin.com/in/celesteurena/')}/>
-          <DescriptionIcon onClick={() => window.open('https://drive.google.com/file/d/1GqhmS3FrLpN8TvWh6FNjL3tZVVxyk9oC/view?usp=sharing')}/>
-          <EmailIcon onClick={() => window.open('mailto:celeste.urena03@gmail.com')}/>
-          <GithubIcon onClick={() => window.open('https://github.com/CelesTech03')}/>
+          <p>I’m a senior studying Computer Science at Hunter College, based in New York. I've always had a knack for problem
+            solving and math. My passion lies in computers and I’m inspired by how technology changes the world. Building software
+            that can change people’s everyday lives for the better is something that I would like to be a part of.</p>
+          <LinkedInIcon onClick={() => window.open('https://www.linkedin.com/in/celesteurena/')} />
+          <DescriptionIcon onClick={() => window.open('https://drive.google.com/file/d/1GqhmS3FrLpN8TvWh6FNjL3tZVVxyk9oC/view?usp=sharing')} />
+          <EmailIcon onClick={() => window.open('mailto:celeste.urena03@gmail.com')} />
+          <GithubIcon onClick={() => window.open('https://github.com/CelesTech03')} />
         </div>
       </div>
 
       <div className='projects'>
         <h1>My Projects</h1>
         <div className='projectList'>
-          <ProjectItem name="Book Finder App" image={Bookworm} />
-          <ProjectItem name="Employee Management System" image={Ems} />
-          <ProjectItem name="Game Website" image={Droplet} />
-          <ProjectItem name="Restaurant Website" image={Restaurant} />
-          <ProjectItem name="Twitter Clone App" image={Twitter} />
-          <ProjectItem name="Instagram Clone App" image={Instagram} />
+          <a href='https://github.com/CelesTech03/Bookworm' target="_blank">
+            <ProjectItem name="Book Finder App" image={Bookworm} /></a>
+          <a href='https://github.com/TienNguyen93/ems-frontend' target="_blank">
+            <ProjectItem name="Employee Management System" image={Ems} /></a>
+          <a href='https://github.com/CelesTech03/Droplet' target="_blank">
+            <ProjectItem name="Game Website" image={Droplet} /></a>
+          <a href='https://github.com/CelesTech03/Restaurant-Website' target="_blank">
+            <ProjectItem name="Restaurant Website" image={Restaurant} /></a>
+          <a href='https://github.com/CelesTech03/Twitter-Clone' target="_blank">
+            <ProjectItem name="Twitter Clone App" image={Twitter} /></a>
+          <a href='https://github.com/CelesTech03/Instagram-Clone' target="_blank">
+            <ProjectItem name="Instagram Clone App" image={Instagram} /></a>
         </div>
       </div>
     </div>
